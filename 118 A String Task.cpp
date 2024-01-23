f021989f2c40f12b23de789c7a87dea3a5c11f2c
Md. Toukir Ahmed <toukirahmed876@gmail.com>
2024-01-24 01:39:35 +0600

    11A codeforce

---
+++ 118 A String Task.cpp
@@ -0,0 +1,16 @@
+//118 A 'String task'codeforce
+#include<iostream>
+using namespace std;
+int main(){
+    string t;
+    cin >> t;
+    for(int i=0; i<t.size();i++){
+        char c= tolower(t[i]);
+        if(c=='a'|| c=='e'|| c=='i'||c=='o'||c=='u'||c=='y'){
+            continue;
+        }
+        else{
+          cout<<"."<<c;  
+        } 
+    }
+}
