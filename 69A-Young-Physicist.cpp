8e96dbc5aeef64bc02192cfde86e8dc86edd56c4
Toukir Ahmed <102986902+toukirahmedtushar@users.noreply.github.com>
2024-01-24 03:37:05 +0600

    Update 69A-Young-Physicist.cpp

--- 69A-Young-Physicist.cpp
+++ 69A-Young-Physicist.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int n;
     cin>> n;
-    int m[n][2];
+    int m[n][3];
     for (int i=0;i<n;i++){
         for (int j=0;j<3;j++){
             cin>> m[i][j];
@@ -18,5 +18,4 @@ int main(){
     if(v==0)
         cout<< "YES";
     else cout<<"NO";
-
 }
